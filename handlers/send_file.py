63cef00197911f7d12938e2c59e463de679b2bd0
demouser777 <116957723+demouser777@users.noreply.github.com>
2023-09-08 21:47:54 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -12,7 +12,7 @@ from handlers.helpers import str_to_b64
 
 async def reply_forward(message: Message, file_id: int):
     try:
-        r =await message.reply_text(
+        r = await message.reply_text(
             f"**Files will be Deleted After 10 min ⏰.Please forward and save them**.",
             disable_web_page_preview=True,
             quote=True
